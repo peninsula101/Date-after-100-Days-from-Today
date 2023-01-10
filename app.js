5034d96b2c6396428473da920610c7a717c0c603
peninsula101 <108569220+peninsula101@users.noreply.github.com>
2023-01-10 11:44:58 +0530

    Update app.js
    get dynamically today's date and add 100 days to it

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  const result = addDays(new Date(2023, 0, 10), 100);
+  /*const result = addDays(new Date(2023, 0, 10), 100);
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );*/
+
+  let date = new Date();
+  /*response.send(
+    `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+  );*/
+  const result = addDays(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    100
+  );
   response.send(
     `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
   );
